7b61a79953ce120c51644ac421ff36751d533757
Denzel97 <denzelwash97@gmail.com>
2019-03-26 05:04:05 +0300

    added my app to settings

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mtaa.apps.MtaaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
