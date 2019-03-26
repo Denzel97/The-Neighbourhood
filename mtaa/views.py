a014c2f465aadddab25e6f1a42d038008c845d50
Denzel97 <denzelwash97@gmail.com>
2019-03-26 06:29:32 +0300

    removed the dummy data

--- mtaa/views.py
+++ mtaa/views.py
@@ -1,24 +1,25 @@
 from django.shortcuts import render
+from .models import Post
 
 
-posts = [
-{
-    'author': 'Denzel97',
-    'title': 'Blog Post 1',
-    'content': 'First post content',
-    'date_posted': 'August 27, 2018'
-},
-{
-    'author': 'Denzel',
-    'title': 'Blog Post 2',
-    'content': 'Second post content',
-    'date_posted': 'August 28, 2018'
-}
-]
+# posts = [
+# {
+#     'author': 'Denzel97',
+#     'title': 'Blog Post 1',
+#     'content': 'First post content',
+#     'date_posted': 'August 27, 2018'
+# },
+# {
+#     'author': 'Denzel',
+#     'title': 'Blog Post 2',
+#     'content': 'Second post content',
+#     'date_posted': 'August 28, 2018'
+# }
+# ]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'mtaa/home.html', context)
 
